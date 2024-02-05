5ec2bd060f64f1b3422e5c820b61800e6bbaa872
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-02-05 17:24:04 +0000

    populated tables

--- kmdb.rb
+++ kmdb.rb
@@ -84,6 +84,82 @@ Role.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+puts "There are #{Studio.all.count} studios"
+new_studio = Studio.new 
+new_studio["name"] = "Warner Bros."
+new_studio.save
+
+warner = Studio.find_by({ "name" => "Warner Bros." })
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year released"] = "2005"
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner ["id"]
+new_movie.save
+
+new_movie2 = Movie.new
+new_movie2["title"] = "The Dark Knight"
+new_movie2["year released"] = "2008"
+new_movie2["rated"] = "PG-13"
+new_movie2["studio_id"] = warner ["id"]
+new_movie2.save
+
+new_movie3 = Movie.new
+new_movie3["title"] = "The Dark Knight Rises"
+new_movie3["year released"] = "2012"
+new_movie3["rated"] = "PG-13"
+new_movie3["studio_id"] = warner ["id"]
+new_movie3.save
+
+puts "There are #{Movie.all.count} movies"
+
+
+new_actor = Actor.new 
+new_actor["name"] = " Christian Bale"
+new_actor.save 
+
+new_actor2 = Actor.new 
+new_actor2["name"] = "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new 
+new_actor3["name"] = "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new 
+new_actor4["name"] = "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new 
+new_actor5["name"] = "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new 
+new_actor6["name"] = "Heath Ledger"
+new_actor6.save
+
+new_actor7 = Actor.new 
+new_actor7["name"] = "Aaron Eckhart"
+new_actor7.save
+
+new_actor8 = Actor.new 
+new_actor8["name"] = "Maggie Gyllenhaal"
+new_actor8.save
+
+new_actor9 = Actor.new 
+new_actor9["name"] = "Tom Hardy"
+new_actor9.save
+
+new_actor10 = Actor.new 
+new_actor10["name"] = "Joseph Gordon-Levitt"
+new_actor10.save
+
+new_actor11 = Actor.new 
+new_actor11["name"] = "Anne Hathaway"
+new_actor11.save
+
+puts "There are #{Actor.all.count} actors"
+
 # Prints a header for the movies output
 puts "Movies"
 puts "======"
